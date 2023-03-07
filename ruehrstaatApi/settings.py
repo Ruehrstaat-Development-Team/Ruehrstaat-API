744cccc8c801c0b799b7b87592f7de5eb955580c
Mats <mats@mtn-media.de>
2023-03-07 11:09:08 +0100

    Version bump to 1.0.4 / ready for main release

--- ruehrstaatApi/settings.py
+++ ruehrstaatApi/settings.py
@@ -8,7 +8,7 @@ config = ConfigParser()
 config.read('config.cfg')
 
 WEBAPP_BRANCH = "BETA"
-WEBAPP_VERSION = "1.0.3"
+WEBAPP_VERSION = "1.0.4"
 
 WEBAPP_NAME = config.get('CUSTOMIZATION', 'WEBAPP_NAME')
 
