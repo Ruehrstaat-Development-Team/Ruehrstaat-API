6b0a6023267267d1890d7f3a2508b6c2c738e83f
Mats Stottmeister <mats@mtn-media.de>
2024-01-24 01:07:15 +0100

    cleanup for rewrite

--- webinterface/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import home, editCarrier, seeCarrier
-
-urlpatterns = [
-    path('', home, name="home"),
-    path('editCarrier/<int:carrierID>', editCarrier, name="editCarrier"),
-    path('seeCarrier/<int:carrierID>', seeCarrier, name="seeCarrier"),
-]
