07f2f1b6d91a26b0b76c93c0bc5ee8aeb020c064
Mats Stottmeister <mats@mtnmedia.group>
2025-05-31 00:06:10 +0200

    fix: leaking data

--- serialize/carrier_serializer.go
+++ serialize/carrier_serializer.go
@@ -25,10 +25,12 @@ func (s *CarrierSerializer) Serialize(carrier entities.Carrier) interface{} {
 	}
 
 	if carrier.Owner != nil {
-		obj.Add("ownerId", carrier.Owner.ID)
 		obj.Add("owner", carrier.Owner.CmdrName)
-		if !s.Limited && carrier.Owner.DiscordId != nil {
-			obj.Add("ownerDiscordId", carrier.Owner.DiscordId)
+		if !s.Limited {
+			obj.Add("ownerId", carrier.Owner.ID)
+			if carrier.Owner.DiscordId != nil {
+				obj.Add("ownerDiscordId", carrier.Owner.DiscordId)
+			}
 		}
 	}
 
