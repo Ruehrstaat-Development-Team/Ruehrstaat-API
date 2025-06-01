52da6deaefdb7a01911dd7e97d163ec4f394e50b
Mats Stottmeister <mats@mtnmedia.group>
2025-06-01 23:45:03 +0200

    fix

--- api/public/carrier.go
+++ api/public/carrier.go
@@ -43,14 +43,7 @@ func publicGetAllCarriers(c *gin.Context) {
 }
 
 func publicGetAllCarriersSortedByCategory(c *gin.Context) {
-	// returns json {
-	// 	"flagship": [Carrier, Carrier, ...],
-	// 	"freighter": [Carrier, Carrier, ...],
-	// 	"supportvessel": [Carrier, Carrier, ...],
-	// 	"other": [Carrier, Carrier, ...]
-	// }
-
-	carriers := map[string][]entities.Carrier{
+	carriers := map[string][]interface{}{
 		"flagship":      {},
 		"freighter":     {},
 		"supportvessel": {},
@@ -62,17 +55,27 @@ func publicGetAllCarriersSortedByCategory(c *gin.Context) {
 		c.JSON(404, gin.H{"error": "Carriers not found"})
 		return
 	}
+
+	flagships := []entities.Carrier{}
+	freighters := []entities.Carrier{}
+	supportVessels := []entities.Carrier{}
+	other := []entities.Carrier{}
 	for _, carrier := range tmpCarriers {
 		switch carrier.Category {
 		case entities.CarrierCategoryFlagship:
-			carriers["flagship"] = append(carriers["flagship"], carrier)
+			flagships = append(flagships, carrier)
 		case entities.CarrierCategoryFreighter:
-			carriers["freighter"] = append(carriers["freighter"], carrier)
+			freighters = append(freighters, carrier)
 		case entities.CarrierCategorySupportVessel:
-			carriers["supportvessel"] = append(carriers["supportvessel"], carrier)
+			supportVessels = append(supportVessels, carrier)
 		default:
-			carriers["other"] = append(carriers["other"], carrier)
+			other = append(other, carrier)
 		}
 	}
+	carriers["flagship"] = serialize.DoArray(&serialize.CarrierSerializer{Limited: true, Full: false}, flagships)
+	carriers["freighter"] = serialize.DoArray(&serialize.CarrierSerializer{Limited: true, Full: false}, freighters)
+	carriers["supportvessel"] = serialize.DoArray(&serialize.CarrierSerializer{Limited: true, Full: false}, supportVessels)
+	carriers["other"] = serialize.DoArray(&serialize.CarrierSerializer{Limited: true, Full: false}, other)
+
 	c.JSON(200, carriers)
 }
